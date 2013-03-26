3e8dd37091b2592f8e4872caff29a2cb3605161c
Anton Gerasimov <me@zyxmasta.com>
2013-03-26 06:11:25 +0000

    create new backend module

---
+++ lib/Plack/Middleware/Throttle/Lite/Backend/Redis.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::Throttle::Lite::Backend::Redis;
+
+# ABSTRACT: Plack::Middleware::Throttle::Lite::Backend::Redis isa blah-blah-blah..
+
+use strict;
+use warnings;
+use 5.01001;
+use Carp ();
+
+# VERSION
+# AUTHORITY
+
+
+1; # End of Plack::Middleware::Throttle::Lite::Backend::Redis
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 SYNOPSYS
+
+=head1 DESCRIPTION
+
+=head1 METHODS/SUBROUTINES
+
+=head1 BUGS
+
+=head1 SEE ALSO
+
+=cut
