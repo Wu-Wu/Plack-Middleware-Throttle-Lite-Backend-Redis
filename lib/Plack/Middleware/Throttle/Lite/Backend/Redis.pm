8a0530923c9545e233d883ffa90438ff9d66aabb
Anton Gerasimov <me@zyxmasta.com>
2013-03-26 08:08:55 +0000

    added connection making sub and its tests

--- lib/Plack/Middleware/Throttle/Lite/Backend/Redis.pm
+++ lib/Plack/Middleware/Throttle/Lite/Backend/Redis.pm
@@ -1,15 +1,58 @@
 package Plack::Middleware::Throttle::Lite::Backend::Redis;
 
-# ABSTRACT: Plack::Middleware::Throttle::Lite::Backend::Redis isa blah-blah-blah..
+# ABSTRACT: Redis-driven storage backend for Throttle-Lite
 
 use strict;
 use warnings;
-use 5.01001;
+use feature ':5.10';
 use Carp ();
+use parent 'Plack::Middleware::Throttle::Lite::Backend::Abstract';
+use Redis 1.955;
 
 # VERSION
 # AUTHORITY
 
+__PACKAGE__->mk_attrs(qw(redis rdb ttl));
+
+sub init {
+    my ($self, $args) = @_;
+
+    my $croak = sub { Carp::croak $_[0] };
+
+    if (!defined $args->{server} && !defined $args->{sock}) {
+        $croak->("Settings should include either server or sock parameter!");
+    }
+
+    my %expire = ('req/hour' => 3600, 'req/day' => 86400);
+
+    my %options = (
+        debug     => $args->{debug}     || 0,
+        reconnect => $args->{reconnect} || 10,
+        every     => $args->{every}     || 100,
+    );
+
+    $options{password} = $args->{password} if $args->{password};
+
+    if (defined $args->{sock}) {
+        $croak->("Nonexistent redis socket ($args->{sock})!") unless -e $args->{sock} && -S _;
+    }
+
+    if (defined $args->{server}) {
+        $croak->("Expected 'hostname:port' for parameter server!") unless $args->{server} =~ /(.*)\:(\d+)/;
+    }
+
+    if (defined $options{sock}) {
+        $options{sock} = $args->{sock};
+    }
+    else {
+        $options{server} = $args->{server};
+    }
+
+    $self->rdb($args->{database} || 0);
+    $self->ttl($args->{expire} || $expire{$self->units});
+
+    $self->redis(Redis->new(%options));
+}
 
 1; # End of Plack::Middleware::Throttle::Lite::Backend::Redis
 
@@ -21,12 +64,30 @@ __END__
 
 =head1 SYNOPSYS
 
+    # inside your app.psgi
+    enable 'Throttle::Lite',
+        backend => [
+            'Redis' => {
+                server   => 'redis.example.com:6379',
+                database => 1,
+                password => 'VaspUtnuNeQuiHesGapbootsewWeonJadacVebEe'
+            }
+        ];
+
 =head1 DESCRIPTION
 
-=head1 METHODS/SUBROUTINES
+=head1 OPTIONS
+
+=head1 METHODS
 
 =head1 BUGS
 
 =head1 SEE ALSO
+
+L<Redis>
+
+L<Plack::Middleware::Throttle::Lite>
+
+L<Plack::Middleware::Throttle::Lite::Backend::Abstract>
 
 =cut
---
+++ t/01-prepare.t
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+use Plack::Test;
+use Plack::Builder;
+use HTTP::Request::Common;
+use Test::More;
+use Plack::Middleware::Throttle::Lite::Backend::Redis;
+
+can_ok 'Plack::Middleware::Throttle::Lite::Backend::Redis', qw(
+    redis
+    reqs_done
+    increment
+    rdb
+    ttl
+);
+
+# simple application
+my $app = sub {
+    [
+        200,
+        [ 'Content-Type' => 'text/html' ],
+        [ '<html><body>OK</body></html>' ]
+    ];
+};
+
+#
+# catch exceptions
+#
+eval { $app = builder { enable 'Throttle::Lite', backend => 'Redis'; $app } };
+like $@, qr|Settings should include either server or sock parameter|, 'Failed without options';
+
+eval { $app = builder { enable 'Throttle::Lite', backend => [ 'Redis' => {} ]; $app } };
+like $@, qr|Settings should include either server or sock parameter|, 'Failed without mandatory params in passed options';
+
+eval { $app = builder { enable 'Throttle::Lite', backend => [ 'Redis' => {server => 'foo'} ]; $app } };
+like $@, qr|Expected 'hostname:port'|, 'Invalid server parameter exception';
+
+SKIP: {
+    skip 'Unix specific test', 1 if $^O eq 'MSWin32';
+    eval { $app = builder { enable 'Throttle::Lite', backend => [ 'Redis' => {sock => '/bogus.sock'} ]; $app } };
+    like $@, qr|Nonexistent redis socket|, 'Invalid sock parameter exception';
+}
+
+done_testing();
