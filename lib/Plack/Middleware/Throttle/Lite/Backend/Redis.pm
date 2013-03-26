c575431c27fd09a44c1f2199742e8a85b1c41ae5
Anton Gerasimov <me@zyxmasta.com>
2013-03-26 15:56:22 +0000

    updated module documentation

--- lib/Plack/Middleware/Throttle/Lite/Backend/Redis.pm
+++ lib/Plack/Middleware/Throttle/Lite/Backend/Redis.pm
@@ -93,11 +93,76 @@ __END__
 
 =head1 DESCRIPTION
 
+This is implemetation of the storage backend for B<Plack::Middleware::Throttle::Lite>. It uses redis-server
+to hold throttling data, automatically sets expiration time for stored keys to save memory consumption.
+
 =head1 OPTIONS
+
+This storage backend must be configured in order to use. All options should be passed as a hash reference. The
+following options are available to tune it for your needs.
+
+=head2 server
+
+A string consist of a hostname (or an IP address) and port number (delimited with a colon) of the redis-server
+instance to connect to. You have to point either this one or L</sock>.
+B<Warning!> This option has lower priority than L</sock>.
+
+=head2 sock
+
+A unix socket path of the redis-server instance to connect to. You have to point either this one or L</server>.
+B<Warning!> This option has higher priority than L</server>.
+
+=head2 database
+
+A redis-server database number to store throttling data. Not obligatory option. If this one omitted then value B<0> will
+be assigned.
+
+=head2 password
+
+Password string for redis-server's AUTH command to processing any other commands. Optional. Check the redis-server
+manual for directive I<requirepass> if you would to use redis internal authentication.
+
+=head2 reconnect
+
+A time (in seconds) to re-establish connection to the redis-server before an exception will be raised. Not required.
+Default value is B<10> sec.
+
+=head2 every
+
+Interval (in milliseconds) after which will be an attempt to re-establish lost connection to the redis-server. Not required.
+Default value is B<100> ms.
+
+=head2 debug
+
+Enables debug information to STDERR, including all interactions with the redis-server. Not required.
+Default value is B<0> (disabled).
 
 =head1 METHODS
 
+=head2 redis
+
+Returns a redis connection handle.
+
+=head2 rdb
+
+A redis database number to store data.
+
+=head2 init
+
+See L<Plack::Middleware::Throttle::Lite::Backend::Abstract/"ABSTRACT METHODS">
+
+=head2 reqs_done
+
+See L<Plack::Middleware::Throttle::Lite::Backend::Abstract/"ABSTRACT METHODS">
+
+=head2 increment
+
+See L<Plack::Middleware::Throttle::Lite::Backend::Abstract/"ABSTRACT METHODS">
+
 =head1 BUGS
+
+Please report any bugs or feature requests through the web interface at
+L<https://github.com/Wu-Wu/Plack-Middleware-Throttle-Lite-Backend-Redis/issues>
 
 =head1 SEE ALSO
 
